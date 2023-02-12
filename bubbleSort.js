a5a137dbf7ee616cdbdf66a3c28ba1b10928d945
Philip Norton <philipnorton42@gmail.com>
2023-02-12 22:07:22 +0000

    Adding answers for the sort algorithms.

--- bubbleSort.js
+++ bubbleSort.js
@@ -8,7 +8,16 @@
  *   The sorted array.
  */
 function bubbleSort(array) {
-  // todo: write bubble sort.
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const x = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = x;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
--- quickSort.js
+++ quickSort.js
@@ -8,7 +8,22 @@
  *   The sorted array.
  */
 function quickSort(array) {
-    // todo: write quick sort.
+  if (array.length === 0) {
+    return array;
+  }
+
+  const k = array[0];
+  const x = [];
+  const y = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] <= k) {
+      x.push(array[i]);
+    } else {
+      y.push(array[i]);
+    }
+  }
+  return quickSort(x).concat([k]).concat(quickSort(y));
 }
 
 module.exports = quickSort;
